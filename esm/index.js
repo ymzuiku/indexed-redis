e60e85d31e5ae26b1d7e5461adfd3b5b42e9c1de
Pillar Liang <ymblender@gmail.com>
2024-02-25 05:16:27 -0800

    feat: update version

--- esm/index.js
+++ esm/index.js
@@ -1,15 +1,76 @@
+// node_modules/throttle-debounce/esm/index.js
+var throttle = function(delay, callback, options) {
+  var _ref = options || {}, _ref$noTrailing = _ref.noTrailing, noTrailing = _ref$noTrailing === undefined ? false : _ref$noTrailing, _ref$noLeading = _ref.noLeading, noLeading = _ref$noLeading === undefined ? false : _ref$noLeading, _ref$debounceMode = _ref.debounceMode, debounceMode = _ref$debounceMode === undefined ? undefined : _ref$debounceMode;
+  var timeoutID;
+  var cancelled = false;
+  var lastExec = 0;
+  function clearExistingTimeout() {
+    if (timeoutID) {
+      clearTimeout(timeoutID);
+    }
+  }
+  function cancel(options2) {
+    var _ref2 = options2 || {}, _ref2$upcomingOnly = _ref2.upcomingOnly, upcomingOnly = _ref2$upcomingOnly === undefined ? false : _ref2$upcomingOnly;
+    clearExistingTimeout();
+    cancelled = !upcomingOnly;
+  }
+  function wrapper() {
+    for (var _len = arguments.length, arguments_ = new Array(_len), _key = 0;_key < _len; _key++) {
+      arguments_[_key] = arguments[_key];
+    }
+    var self = this;
+    var elapsed = Date.now() - lastExec;
+    if (cancelled) {
+      return;
+    }
+    function exec() {
+      lastExec = Date.now();
+      callback.apply(self, arguments_);
+    }
+    function clear() {
+      timeoutID = undefined;
+    }
+    if (!noLeading && debounceMode && !timeoutID) {
+      exec();
+    }
+    clearExistingTimeout();
+    if (debounceMode === undefined && elapsed > delay) {
+      if (noLeading) {
+        lastExec = Date.now();
+        if (!noTrailing) {
+          timeoutID = setTimeout(debounceMode ? clear : exec, delay);
+        }
+      } else {
+        exec();
+      }
+    } else if (noTrailing !== true) {
+      timeoutID = setTimeout(debounceMode ? clear : exec, debounceMode === undefined ? delay - elapsed : delay);
+    }
+  }
+  wrapper.cancel = cancel;
+  return wrapper;
+};
+var debounce = function(delay, callback, options) {
+  var _ref = options || {}, _ref$atBegin = _ref.atBegin, atBegin = _ref$atBegin === undefined ? false : _ref$atBegin;
+  return throttle(delay, callback, {
+    debounceMode: atBegin !== false
+  });
+};
+
 // lib/index.ts
-var IndexedRedis = (dbName) => {
+var indexedRedis = (dbName) => {
   const isHaveIndexedDb = typeof window.indexedDB !== "undefined";
   if (!isHaveIndexedDb) {
     console.error("[nano-indexed] [Error] Your browser not have indexedDB, Now use localStorage.");
   }
   let db;
   let lastClearTime = 0;
+  let setExJobs = {};
+  let valueCache = {};
   const initDb = () => {
     return new Promise((res) => {
       if (!db) {
-        const reqDb = window.indexedDB.open(dbName);
+        const reqDb = window.indexedDB.open("indexed-redis");
         reqDb.onerror = console.error;
         reqDb.onsuccess = (event) => {
           if (!db) {
@@ -28,7 +89,21 @@ var IndexedRedis = (dbName) => {
       }
     });
   };
+  const getCacheValue = (key) => {
+    const old = valueCache[key];
+    if (old) {
+      if (old.expire && old.expire < Date.now()) {
+        delete valueCache[key];
+        return;
+      }
+      return old.value;
+    }
+  };
   const get = async (key) => {
+    const cacheValue = getCacheValue(key);
+    if (cacheValue !== undefined) {
+      return cacheValue;
+    }
     clearExpiredItems();
     if (!isHaveIndexedDb) {
       return new Promise((res) => {
@@ -70,9 +145,17 @@ var IndexedRedis = (dbName) => {
     });
   };
   const getAll = async () => {
+    const out2 = {};
+    Object.keys(valueCache).forEach((key) => {
+      const v = valueCache[key];
+      if (v.expire && v.expire < Date.now()) {
+        delete valueCache[key];
+        return;
+      }
+      out2[key] = v.value;
+    });
     if (!isHaveIndexedDb) {
       return new Promise((res) => {
-        const out2 = {};
         const now = Date.now();
         for (let i = 0;i < localStorage.length; i++) {
           const key = localStorage.key(i) || "";
@@ -82,18 +165,19 @@ var IndexedRedis = (dbName) => {
               let item;
               try {
                 item = JSON.parse(itemStr);
+                if (item.expire && now > item.expire) {
+                  localStorage.removeItem(key);
+                  continue;
+                }
+                const realKey = key.replace(`[${dbName}] `, "");
+                if (out2[realKey] === undefined) {
+                  out2[realKey] = item.value;
+                }
               } catch (error) {
-                continue;
               }
-              if (now > item.expiry) {
-                localStorage.removeItem(key);
-                continue;
-              }
-              out2[key.replace(`[${dbName}] `, "")] = item.value;
             }
           }
         }
-        const keys = Object.keys(localStorage);
         res(out2);
       });
     }
@@ -105,34 +189,43 @@ var IndexedRedis = (dbName) => {
         const transaction = db.transaction([dbName]);
         const objectStore = transaction.objectStore(dbName);
         const request = objectStore.getAll();
+        const needDelete = [];
         request.onsuccess = (event) => {
           const data = event.target.result;
-          const out2 = {};
+          const now = Date.now();
           data.forEach((v) => {
             Object.keys(v).forEach((key) => {
-              if (key !== "value") {
-                if (v.expire && v.expire < Date.now()) {
+              if (key !== "value" && key !== "expire") {
+                if (v.expire && v.expire < now) {
+                  needDelete.push(key);
                   return;
                 }
-                out2[key] = v.value;
+                if (out2[key] === undefined) {
+                  out2[key] = v.value;
+                }
               }
             });
           });
           res(out2);
+          setTimeout(() => {
+            needDelete.forEach((key) => {
+              del(key);
+            });
+          });
         };
       } else {
-        res({});
+        res(out2);
       }
     });
   };
-  const setEx = async (key, expireSecond, obj) => {
+  const setEx = async (key, expireMillisecond, obj) => {
     clearExpiredItems();
     const theObj = obj;
     if (!isHaveIndexedDb) {
-      return new Promise((res, rej) => {
+      return new Promise((res) => {
         localStorage.setItem(`[${dbName}] ${key}`, JSON.stringify({
           value: theObj,
-          expire: expireSecond ? Date.now() + expireSecond * 1000 : 0
+          expire: expireMillisecond ? Date.now() + expireMillisecond : 0
         }));
         res(obj);
       });
@@ -147,7 +240,7 @@ var IndexedRedis = (dbName) => {
         const data = {
           [key]: key,
           value: theObj,
-          expire: expireSecond ? Date.now() + expireSecond * 1000 : 0
+          expire: expireMillisecond ? Date.now() + expireMillisecond : 0
         };
         const request = objectStore.put(data, key);
         request.onerror = (err) => {
@@ -162,26 +255,27 @@ var IndexedRedis = (dbName) => {
       }
     });
   };
-  const assign = async (key, obj) => {
+  const assignEx = async (key, expireMillisecond, obj) => {
     if (typeof obj !== "object") {
       throw new Error("[NanoIndexed.assign] assign need is object");
     }
     const old = await out.get(key);
     if (!old) {
-      await out.set(key, obj);
+      out.setEx(key, expireMillisecond, obj);
       return obj;
     }
     if (typeof old !== "object") {
       return old;
     }
     const next = Object.assign(old, obj);
-    await out.set(key, next);
+    out.setEx(key, expireMillisecond, next);
     return next;
   };
-  const remove = async (key) => {
+  const del = async (key) => {
+    delete valueCache[key];
     if (!isHaveIndexedDb) {
       return new Promise((res) => {
-        localStorage.removeItem((key || 1).toString());
+        localStorage.removeItem(`[${dbName}] ${key}`);
         res(undefined);
       });
     }
@@ -212,23 +306,73 @@ var IndexedRedis = (dbName) => {
     return getAll();
   };
   clearExpiredItems();
+  const flushDb = async () => {
+    const all = await getAll();
+    await Promise.all(Object.keys(all).map((key) => {
+      return del(key);
+    }));
+  };
+  let reduceValueCache = 0;
+  const runSetExJobs = debounce(500, () => {
+    reduceValueCache++;
+    if (reduceValueCache > 200) {
+      reduceValueCache = 0;
+      Object.keys(valueCache).forEach((k) => {
+        const v = valueCache[k];
+        if (v.expire && v.expire < Date.now()) {
+          delete valueCache[k];
+        }
+      });
+      const keys2 = Object.keys(valueCache);
+      if (keys2.length > 500) {
+        valueCache = {};
+      }
+    }
+    const keys = Object.keys(setExJobs);
+    if (keys.length === 0) {
+      return;
+    }
+    keys.forEach((key) => {
+      const job = setExJobs[key];
+      if (job) {
+        setEx(key, job.expire, job.value);
+      }
+    });
+    setExJobs = {};
+  });
   const out = {
-    setEx: async (key, expireSecond, obj) => {
-      return setEx(key, expireSecond, obj);
+    set: async (key, value) => {
+      out.setEx(key, 0, value);
     },
-    set: async (key, obj) => {
-      return setEx(key, 0, obj);
+    setEx: async (key, expireMillisecond, value) => {
+      const now = Date.now();
+      setExJobs[key] = {
+        expire: expireMillisecond ? now + expireMillisecond : 0,
+        value
+      };
+      valueCache[key] = {
+        expire: expireMillisecond ? now + expireMillisecond : 0,
+        value
+      };
+      runSetExJobs();
     },
-    get,
+    get: async (key) => {
+      const old = getCacheValue(key);
+      if (old !== undefined) {
+        return old;
+      }
+      return get(key);
+    },
     getAll,
-    assign,
-    remove,
+    assignEx,
+    del,
+    flushDb,
     clearExpiredItems
   };
   return out;
 };
 export {
-  IndexedRedis
+  indexedRedis
 };
 
-//# debugId=D75DC001B945F33464756e2164756e21
+//# debugId=17E5C0980F6A924564756e2164756e21
